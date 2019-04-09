c9fcfe3c61e41f9b51b675c04924e1b67981ee76
Ellil1 <emile@douilhet.com>
2019-04-09 21:47:24 +0100

    OKAY BABES

--- app/src/main/java/com/example/ourapp/MainActivity.java
+++ app/src/main/java/com/example/ourapp/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.ourapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity<calendar> extends AppCompatActivity {
+    private TextView dateTimeDisplay;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // hey
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+
+    }
+
+    protected void addDate(){
+        TextView dateTimeDisplay = (TextView) findViewById(R.id.text_date_display);
     }
 }
+
